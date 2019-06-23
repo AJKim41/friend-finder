c56298281ca272a64241c3c43e70b65a4dc6a94e
ajkim41 <ajkim41@gmail.com>
2019-06-22 23:21:03 -0400

    Javscript

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const homeRoutes = require("./app/routing/htmlRoutes");
+const apiRoutes = require("./app/routing/apiRoutes");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(homeRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
